4a902540e461ddc63e0522720ac37e1b68fc4f99
Eric Hutabarat <76461464+erichutabarat@users.noreply.github.com>
2023-11-27 16:16:29 +0700

    Update Features: RecyclerView
    - Update Card Product
    - Add border to all product

--- ModernStore/app/src/main/java/com/modernstore/app/ui/shopping/ProductAdapter.kt
+++ ModernStore/app/src/main/java/com/modernstore/app/ui/shopping/ProductAdapter.kt
@@ -3,10 +3,13 @@ package com.modernstore.app.ui.shopping
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.modernstore.app.R
 import com.modernstore.app.data.model.Product
+import org.w3c.dom.Text
 
 class ProductAdapter(private val products: List<Product>) :
     RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
@@ -28,8 +31,15 @@ class ProductAdapter(private val products: List<Product>) :
         fun bind(product: Product) {
             itemView.findViewById<TextView>(R.id.product_title).text = product.title
             itemView.findViewById<TextView>(R.id.product_price).text = "$${product.price}"
-            // Load image using your preferred image-loading library (e.g., Glide, Picasso)
-            // itemView.findViewById<ImageView>(R.id.productImage)
+            itemView.findViewById<TextView>(R.id.product_caregory).text = product.category
+            val productImage = itemView.findViewById<ImageView>(R.id.product_image)
+            // Load image using Glide
+            Glide.with(itemView)
+                .load(product.image)
+                .placeholder(R.drawable.dummy)
+                .error(R.drawable.dummy)
+                .centerInside()
+                .into(productImage)
         }
     }
 }
