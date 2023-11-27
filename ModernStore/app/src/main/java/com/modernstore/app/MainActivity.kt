f5870f4cfe9f98adbc18457d7bb217268fc052a8
Eric Hutabarat <76461464+erichutabarat@users.noreply.github.com>
2023-11-27 11:26:55 +0700

    Add Features: Bottom Navigation Menu

--- ModernStore/app/src/main/java/com/modernstore/app/MainActivity.kt
+++ ModernStore/app/src/main/java/com/modernstore/app/MainActivity.kt
@@ -4,19 +4,45 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.modernstore.app.ui.account.AccountFragment
+import com.modernstore.app.ui.cart.CartFragment
 import com.modernstore.app.ui.login.LoginActivity
 import com.modernstore.app.ui.register.RegisterActivity
+import com.modernstore.app.ui.shopping.ShoppingFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_page)
-
-        val toLogin: TextView = findViewById(R.id.next_to_login)
-        toLogin.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
+        setContentView(R.layout.activity_main)
+        loadFragment(ShoppingFragment())
+        bottomNav = findViewById(R.id.bottomnav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(ShoppingFragment())
+                    true
+                }
+                R.id.message -> {
+                    loadFragment(CartFragment())
+                    true
+                }
+                R.id.settings -> {
+                    loadFragment(AccountFragment())
+                    true
+                }
+                else -> throw IllegalArgumentException("Unhandled itemId: ${it.itemId}")
+            }
         }
     }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
 }
